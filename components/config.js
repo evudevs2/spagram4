a33d30ec3b98cb3c5f8607fa5e66007ff3227d76
Alex Zordel <alex@zordel.com>
2025-05-15 05:45:58 -0700

    config change for domain

--- components/config.js
+++ components/config.js
@@ -2,7 +2,7 @@
 const LOCAL_URL = 'http://localhost:3005/';
 // const REMOTE_URL = 'https://amazing-jelly-ff8bef.netlify.app/';
 // const REMOTE_URL = 'https://spagram.com/';
-const REMOTE_URL = 'https://spagram2.vercel.app/';
+const REMOTE_URL = 'https://spg2.netlify.app/';
 const REMOTE_URL_OLD = 'https://tristatemassage.com/';
 // const REMOTE_URL = 'https://app.spagram.com/';
 
